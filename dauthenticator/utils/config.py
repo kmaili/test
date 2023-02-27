f58b47fc859c7e6c5355d5223fe4e2d1556a364f
NChamroukhi <nchamroukhi@kaisensdata.fr>
2023-02-27 09:01:39 +0000

    fix media name

--- dauthenticator/utils/config.py
+++ dauthenticator/utils/config.py
@@ -4,6 +4,7 @@ import logging
 WORKING_DIR = os.getcwd()
 
 # logging
+os.makedirs(os.path.join(WORKING_DIR, 'dauthenticator/logs'), exist_ok=True)
 logging_folder = os.path.join(WORKING_DIR, 'dauthenticator/logs', 'dauthenticator.log')
 logging_level = logging.INFO
 logging_format = '%(asctime)s: %(name)s: [%(levelname)s]: %(message)s'
