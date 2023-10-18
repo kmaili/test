831f2dd24987ddf60a2b99d05d91a67648d7f252
meriem <mmkhinini@kaisensdata.fr>
2023-10-18 14:46:03 +0200

    UPFATE STATICFILES_STORAGE

--- config/settings/base.py
+++ config/settings/base.py
@@ -18,7 +18,7 @@ if READ_DOT_ENV_FILE:
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool("DJANGO_DEBUG", False)
+DEBUG = env.bool("DJANGO_DEBUG", True)
 
 #TIME_ZONE = "Europe/Paris"
 TIME_ZONE = "UTC"
